16777d822d1d5649ca12de8135f829b4572faa94
tikous <1120531060@qq.com>
2024-05-07 13:15:23 +0800

    add, delete fucntion

--- reminders-app/src/App.tsx
+++ reminders-app/src/App.tsx
@@ -1,11 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import ReminderList from './components/ReminderList';
+import Reminder from './models/reminder';
+import reminderService from './services/reminder'
+import NewReminder from './components/NewReminder';
 
 function App() {
+  const [reminders, setReminders] = useState<Reminder[]>([])
+
+  useEffect(() => {
+    loadReminders()
+  }, [])
+
+  const loadReminders = async () => {
+    const reminders = await reminderService.getReminders()
+    setReminders(reminders)
+  }
+
+  const removeReminder = (id: number) => {
+    console.log(id)
+    setReminders(reminders.filter(reminder => reminder.id !== id))
+  }
+
+  const addReminder = async (title: string) => {
+    const newReminder = await reminderService.addReminder(title)
+    setReminders([newReminder, ...reminders])
+  }
+
   return (
     <div className="App">
-      <button className="btn btn-primary">Click me</button>
+      <NewReminder onAddReminder={addReminder} />
+      <ReminderList items={reminders} onRemoveReminder={removeReminder} />
     </div>
   );
 }
